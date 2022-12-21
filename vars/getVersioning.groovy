c382435d36b3fbcc5163205f8b28eb3a9012107e
JisungPark <jisung9870@gmail.com>
2022-12-21 11:51:24 +0900

    fix(jenkins): modify

--- vars/getVersioning.groovy
+++ vars/getVersioning.groovy
@@ -40,7 +40,7 @@ def call(Map git_info = [:]) {
         }
     }
 
-    GIT_URL = env.GIT_URL.findAll("(http|https)?:\\/\\/(\\S+)")[2]
+    GIT_URL = env.GIT_URL.findAll("(http|https)?:\\/\\/(\\S+)")[1]
     withCredentials([gitUsernamePassword(credentialsId: git_info.credentials, gitToolName: 'git-tool')]) {
         sh "git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${GIT_URL} HEAD:${env.GIT_BRANCH} --tags"
     }
